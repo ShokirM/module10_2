5749761ddc78e39c08d9315b55ecb5483178442b
Shokir <m.shokir@ya.ru>
2022-06-26 16:45:13 +0500

    Готов к слиянию

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module bank
+module github.com/ShokirM/module10_2.git
 
 go 1.18
--- pkg/bank/stats/stats.go
+++ pkg/bank/stats/stats.go
@@ -1,15 +1,22 @@
 package stats
 
-import "bank/pkg/bank/types"
+import "github.com/ShokirM/module10_2"
 
 func Avg(payments []types.Payment) types.Money {
-summ := 0
-for _, pay := range payments {
-	summ += int(pay.Amount)
+	summ := 0
+	for _, pay := range payments {
+		summ += int(pay.Amount)
+	}
+	avgAmount := summ / len(payments)
+	return types.Money(avgAmount)
 }
-avgAmount := summ / len(payments)
-return types.Money(avgAmount)
+
+func TotalInCategory(payments []types.Payment, category types.Category) types.Money {
+	summ := 0
+	for _, pay := range payments {
+		if pay.Category == category {
+			summ += int(pay.Amount)
+		}
+	}
+	return types.Money(summ)
 }
-
-
-
